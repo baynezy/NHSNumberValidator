5784fbc03ef24e0b6f647fe7579502276028f664
Simon Baynes <baynezy@gmail.com>
2022-10-20 17:16:51 +0100

    Add project skeleton

---
+++ test/NHS.NumberValidator.Test/Drivers/Driver.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace NHS.NumberValidator.Test.Drivers
+{
+    public class Driver
+    {
+    }
+}
---
+++ test/NHS.NumberValidator.Test/Hooks/Hook.cs
@@ -0,0 +1,10 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace NHS.NumberValidator.Test.Hooks
+{
+    [Binding]
+    public class Hooks
+    {
+    }
+}
